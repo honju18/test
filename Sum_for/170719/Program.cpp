6d699a865ff3672f25025195af137f4162192611
이홍주 <honju18@blackfortsec.com>
2017-07-19 18:59:54 +0900

    sum 함수 생성

--- Sum_for/170719/Program.cpp
+++ Sum_for/170719/Program.cpp
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+int Sum(int n)
+{
+	int result = 0, i = 0;
+	for (i = 1; i <= n; i++)
+	{
+		result += i;
+	}
+
+	return result;
+}
+
 int main()
 {
 	int input1 = 0, result = 0, i = 0;
@@ -7,10 +18,7 @@ int main()
 	printf("1 부터 n 까지 합을 구합니다.\nn 값을 입력하세요 : ");
 	scanf("%d", &input1);
 
-	for (i = 1; i <= input1; i++)
-	{
-		result += i;
-	}
+	result = Sum(input1);
 
 	printf("1 ~ %d 합 = %d\n", input1, result);
 	return 0;
