d0e89c03d4a2bb1765d5c53465e3110bbfc43afa
이홍주 <honju18@blackfortsec.com>
2017-07-19 18:29:58 +0900

    Dll Injection 프로젝트

---
+++ InjectDll/InjectDll/Program.cpp
@@ -0,0 +1,76 @@
+#include "windows.h"
+#include "tchar.h"
+#include "tlhelp32.h"
+
+DWORD FindProcessID(LPCTSTR szProcessName)
+{
+	DWORD dwPID = 0xFFFFFFFF;
+	HANDLE hSnapShot = INVALID_HANDLE_VALUE;
+	PROCESSENTRY32 pe;
+
+	pe.dwSize = sizeof(PROCESSENTRY32);
+	hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, NULL);
+
+	Process32First(hSnapShot, &pe);
+	do{
+		if (!_tcsicmp(szProcessName, pe.szExeFile))
+		{
+			dwPID = pe.th32ProcessID;
+			break;
+		}
+	} while (Process32Next(hSnapShot, &pe));
+
+	CloseHandle(hSnapShot);
+
+	return dwPID;
+}
+
+BOOL InjectDll(DWORD dwPID, LPCTSTR szDllPath)
+{
+	HANDLE hProcess = NULL, hThread = NULL;
+	HMODULE hMod = NULL;
+	LPVOID pRemoteBuf = NULL;
+	DWORD dwBufSize = (DWORD)(_tcslen(szDllPath) + 1) * sizeof(TCHAR);
+	LPTHREAD_START_ROUTINE pThreadProc;
+
+	if (!(hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwPID)))
+	{
+		_tprintf(L"failed!! error : [%d]", GetLastError());
+		return FALSE;
+	}
+
+	pRemoteBuf = VirtualAllocEx(hProcess, NULL, dwBufSize, MEM_COMMIT, PAGE_READWRITE);
+
+	WriteProcessMemory(hProcess, pRemoteBuf, (LPVOID)szDllPath, dwBufSize, NULL);
+
+	hMod = GetModuleHandle(L"kernel32.dll");
+	pThreadProc = (LPTHREAD_START_ROUTINE)GetProcAddress(hMod, "LoadLibraryW");
+
+	hThread = CreateRemoteThread(hProcess, NULL, 0, pThreadProc, pRemoteBuf, 0, NULL);
+	WaitForSingleObject(hThread, INFINITE);
+
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
+
+	return TRUE;
+}
+
+int _tmain(int argc, TCHAR *argv[])
+{
+	DWORD PID = 0;
+
+	PID = FindProcessID(argv[1]);
+
+	if (argc != 3)
+	{
+		_tprintf(L"USAGE : %s pid dll_path\n", argv[0]);
+		return 1;
+	}
+
+	if (InjectDll(PID, argv[2]))
+		_tprintf(L"InjectDLL success!!!\n");
+	else
+		_tprintf(L"InjectDLL failed!!!\n");
+
+	return 0;
+}
