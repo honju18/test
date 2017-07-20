728a3b485fb84540e0e51fcf97a49ea34723a308
이홍주 <honju18@blackfortsec.com>
2017-07-20 12:37:59 +0900

    first commit

--- GetLastError/GetLastError/Program.cpp
+++ GetLastError/GetLastError/Program.cpp
@@ -1,6 +1,3 @@
-
-//ErrorStateChange
-
 #include <stdio.h>
 #include <tchar.h>
 #include <Windows.h>
@@ -12,16 +9,10 @@ int _tmain(void)
 		_T("ABC.DAT"), GENERIC_READ, FILE_SHARE_READ,
 		NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL,
 		NULL);
-
-	_tprintf(_T("error code: %d\n"), GetLastError());
-
-	hFile =
-		CreateFile(
-		_T("ABC.DAT"), GENERIC_WRITE, FILE_SHARE_READ,
-		NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL,
-		NULL);
-	
-	_tprintf(_T("error code: %d\n"), GetLastError()); // 오류확인
-
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		_tprintf(_T("error code: %d\n"), GetLastError());
+		return 0;
+	}
 	return 0;
 }
